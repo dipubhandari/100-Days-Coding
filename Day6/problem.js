802bbaacf0e7158f2485fe0361d35a3cc47ab9ba
dipubhandari <dipendrabhandari123456@gmail.com>
2025-06-28 20:52:36 +0100

    Day 7 of 100 Days Chanllenge

---
+++ Day6/problem.js
@@ -0,0 +1,19 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+const input = 1234321;
+const data = String(input).split("");
+
+function findPalin(data) {
+  const input = data;
+  const checkEven = parseInt(input);
+  //   if (!checkEven) {
+  //     console.log("The number is not a palindrone");
+  //   } else {
+  //     console.log(checkEven);
+  //     // const mid = (n-1)/2
+  //   }
+}
+findPalin(data);
---
+++ Day7 - Async Await/AsyncAwait.js
@@ -0,0 +1,8 @@
+console.log("Learnnig Async Await");
+
+// async makes a function return a Promise
+// await makes a function wait for a Promise
+
+const fetchData = async () => {};
+
+fetchData();
