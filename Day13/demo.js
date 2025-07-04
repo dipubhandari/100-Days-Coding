95a039e3ceecee0b0a56dc085a569f6e4b1d379b
dipubhandari <dipendrabhandari123456@gmail.com>
2025-07-04 08:59:44 +0100

    Day 13 add two sum leet code

---
+++ Day13/demo.js
@@ -0,0 +1,9 @@
+const add = 10;
+const addArray = add.toString().split("");
+const firstNum = parseInt(addArray[0]);
+console.log(firstNum);
+console.log(typeof firstNum);
+const secondNum = parseInt(addArray[1]);
+console.log(secondNum);
+console.log(typeof secondNum);
+remainder = parseInt(secondNum[0]);
---
+++ Day13/twoSum.js
@@ -0,0 +1,25 @@
+const l1 = [2, 4, 3];
+const l2 = [5, 6, 4];
+function addTwo(input1, input2) {
+  let remainder = 0;
+  let output = [];
+  // looping
+  for (let i = 0; i < l1.length; i++) {
+    // adding first and second input numbers
+    const add = input1[i] + input2[i] + remainder;
+    // if number is less than 10 than push it as it is
+    if (add < 10) {
+      output.push(add);
+    }
+    // if the number is greater than 9
+    else {
+      const addArray = add.toString().split("");
+      const firstNum = parseInt(addArray[1]);
+      output.push(firstNum);
+      const secondNum = parseInt(addArray[0]);
+      remainder = parseInt(secondNum);
+    }
+  }
+  console.log(output);
+}
+addTwo(l1, l2);
